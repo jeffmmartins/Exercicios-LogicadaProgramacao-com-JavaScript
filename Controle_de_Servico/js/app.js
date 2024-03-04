c57c283e6b13e81feaff63d3c0a17f53b2a482dc
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-03-04 07:12:35 -0300

    commit new update

--- Controle_de_Servico/js/app.js
+++ Controle_de_Servico/js/app.js
@@ -2,8 +2,9 @@ const frm = document.querySelector('form')
 
 const resultado1 = document.querySelector('h2')
 const resultado2 = document.querySelector('h3')
+const resultado3 = document.querySelector('h4')
 
-frm.inAdicionar.addEventListener('click', (e) => {
+frm.inAdicionar.addEventListener("click", (e) => {
   e.preventDefault()
 
   const servico = frm.inServico.value
@@ -19,13 +20,28 @@ frm.inAdicionar.addEventListener('click', (e) => {
   }else{
     localStorage.setItem("nomeServico",servico)
   }
-   
-  
-    
-  
   /*
   const descricao = localStorage.getItem("nomeServico") + ";" + servico
   localStorage.setItem("nomeServico", descricao)
   */
+})
+
+frm.inExecutar.addEventListener("click", (e) => {
+  e.preventDefault()
+  
+  
+  
+  const informacoes = localStorage.getItem("nomeServico").split(";")
+  const primeiroServico = informacoes.shift()
+  localStorage.setItem("nomeServico", informacoes)
+  
+    
+  
+  
+  resultado1.innerText = `Seriço Pendentes: ${informacoes.length}`
+  resultado3.innerText = "Servoço em Execução:"
+  resultado2.innerText = primeiroServico
+
+  
 })
 
